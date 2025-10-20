8575f3270cbc2d98e2804f9d40f4acd31364ec87
catalamarti <marticatalasabate@gmail.com>
2025-10-20 23:44:54 +0100

    checkout

---
+++ R/checkout.R
@@ -0,0 +1,76 @@
+
+#' An
+#'
+#' @param result A `summarised_result` object.
+#' @param file Name of an '.md' file to write the report.
+#'
+#' @return Whether the result object passes the checks or not.
+#' @export
+#'
+#' @examples
+checkout <- function(result,
+                     file = NULL) {
+  # initial check
+  result <- omopgenerics::validateResultArgument(result = result)
+  omopgenerics::assertCharacter(file, length = 1, minNumCharacter = 1, null = TRUE)
+  if (!is.null(file)) {
+    if (!endsWith(x = file, suffix = ".md")) {
+      file <- paste0(file, ".md")
+    }
+  }
+
+  # suppress summary
+  sup <- checkoutSuppress(result = result)
+
+
+}
+
+checkoutSuppress <- function(result) {
+  suppressWarnings(capture.output())
+}
+checkoutSummary <- function(result) {
+  report <- c("# `<summarised_result>`", "")
+
+  # initial summary
+
+  # summarise suppression
+
+  # summarise packages, functions and estimates
+
+  invisible(report)
+}
+showReport <- function(x) {
+  if (interactive()) {
+    # temporary file
+    file <- tempfile(fileext = ".html")
+
+    # compile md
+    x <- paste0(
+      "<!doctype html>\n<html>\n<head>\n<meta charset='utf-8'>\n<title>Preview</title>\n</head>\n<body>\n",
+      commonmark::markdown_html(x),
+      "\n</body>\n</html>"
+    )
+
+    # write to temporary file
+    writeLines(text = x, con = file)
+
+    # visualise report
+    if (rlang::is_installed("rstudioapi")) {
+      if (rstudioapi::isAvailable()) {
+        rstudioapi::viewer(url = file)
+      } else {
+        utils::browseURL(url = file)
+      }
+    } else {
+      utils::browseURL(url = file)
+    }
+  }
+
+  invisible()
+}
+writeReport <- function(x, file) {
+  if (!is.null(file)) {
+    writeLines(text = x, con = file)
+  }
+  invisible()
+}
---
+++ R/shiny.R
@@ -0,0 +1,19 @@
+
+checkoutShiny <- function() {
+
+  # create ui
+  ui <- bslib::page_navbar(
+
+  )
+
+  # upload data ui
+
+  # result suppression
+
+  # result summary
+
+  # result explorer
+
+  # result log
+
+}
