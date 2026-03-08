b105f4116956545af682dd659f945dcf3ee859d1
Martí Català Sabaté <marticatalasabate@gmail.com>
2026-03-08 21:23:41 +0000

    shiny app

---
+++ extras/createShiny.R
@@ -0,0 +1,23 @@
+
+shinyDirectory <- file.path(getwd(), "extras", "shiny")
+unlink(shinyDirectory, recursive = TRUE)
+dir.create(path = shinyDirectory)
+file.copy(
+  from = system.file("shiny", package = "OmopCheckout"),
+  to = file.path(getwd(), "extras"),
+  recursive = TRUE
+)
+
+bp <- file.path(getwd(), "extras", "shiny", "background.md")
+backgoundFile <- c(
+  readLines(con = bp),
+  '',
+  '**!NOTE** that if you upload results to this shiny app you are uploading those results to <shinyapp.io> you can easily set a local version of this shiny app with:',
+  '',
+  '```',
+  '# pak::pkg_install("ohdsi/OmopCheckout")',
+  'library(OmopCheckout)',
+  'checkoutShiny()',
+  '```'
+) |>
+  writeLines(bp)
